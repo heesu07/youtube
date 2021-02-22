8ff41797d9d609b07e97d9c4cac804046527c565
Daniel <42251888+heesu07@users.noreply.github.com>
2021-02-23 10:21:43 +1300

    back to the list after searching

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,32 @@
 
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
-import React, {useCallback, useState, useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
 import Videolist from './components/video_list/video_list';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
   
+  const selectVideo = (video) => {    
+    setSelectedVideo(video);
+    console.log(`selected`);
+  };
+
   const search = query =>{
+    setSelectedVideo(null);
+
     youtube.search(query)
-    .then(videos =>setVideos(videos));    
+    .then(videos =>{
+      setVideos(videos);      
+    });    
   };
 
   useEffect(() => {
     youtube.mostPopular()
     .then(videos =>setVideos(videos));    
-  }, []);
+  }, [selectedVideo]);
 
   
 
@@ -23,7 +34,17 @@ function App({youtube}) {
     <div>
       <SearchHeader onSearch={search} /> 
 
-      <Videolist videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} /> 
+          </div> )
+        }
+        
+        <div className={styles.list}>
+          <Videolist videos={videos} onVideoClick={selectVideo} display ={ selectedVideo ? 'list' : 'grid'}/>
+        </div>
+      </section>
     </div>
   );
 }
